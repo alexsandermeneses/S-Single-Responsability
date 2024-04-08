3878a4871d09f5f4485cc8c197295d6a0d544595
Alex Sander <sandermeneses2000@hotmail.com>
2024-04-08 11:47:58 +0100

    Att

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public void listClient(){
+
+    }
+
+    public void addClient(){
+
+    }
+
+    public void updateClient(){
+
+    }
+
+    public void deleteClient(){
+
+    }
+}
---
+++ src/main/java/org/example/utility/Notify.java
@@ -0,0 +1,9 @@
+package org.example.utility;
+
+public class Notify {
+
+    public void notifyClient(){
+
+    }
+
+}
